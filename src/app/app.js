b8c65b27834909d5b584e5debdf4dee4a75c52de
Sergei Alekseev <sergei.alekseev@corp.mail.ru>
2016-05-17 15:02:06 +0300

    x

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,13 @@
 angular.module('app', ['ngPhotoswipe'])
-    .directive('app', function () {
+    .directive('app', function ($rootScope) {
         return {
             restrict: 'E',
-            templateUrl: 'app.html'
+            templateUrl: 'app.html',
+            link: function(scope){
+                $rootScope.toggleOnline = function(){
+                    $rootScope.offline = !$rootScope.offline;
+                    $rootScope.userOpen = $rootScope.offline
+                }
+            }
         }
     });
