dda6f9173e51ecaf16aaeda4c2ff7e97d80552b2
Sergei Alekseev <sergei.alekseev@corp.mail.ru>
2016-05-17 15:46:49 +0300

    x

--- src/app/app.js
+++ src/app/app.js
@@ -8,6 +8,18 @@ angular.module('app', ['ngPhotoswipe'])
                     $rootScope.offline = !$rootScope.offline;
                     $rootScope.userOpen = $rootScope.offline
                 }
+
+                $rootScope.showSotrudniki = function(){
+                    $rootScope.sotrudniki = true;
+                    $rootScope.history = !$rootScope.sotrudniki;
+                }
+
+                $rootScope.showHistory = function(){
+                    $rootScope.history = true;
+                    $rootScope.sotrudniki = !$rootScope.history;
+                }
+
+                $rootScope.showHistory()
             }
         }
     });
---
+++ src/app/sotr/sotr.js
@@ -0,0 +1,9 @@
+angular.module('app')
+    .directive('sotr', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'sotr/sotr.html',
+            link: function(scope){
+            }
+        }
+    });
